7c7cc4c6936fcba6b40fa3c047e77c0c53cd573f
iwantxseeyxubleed <105248596+iwantxseeyxubleed@users.noreply.github.com>
2024-12-22 20:23:14 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Добавлено для подавления предупреждения
 db = SQLAlchemy(app)
 
 class Partner(db.Model):
